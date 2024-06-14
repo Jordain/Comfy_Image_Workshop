f8e661809ee0730c057a7831604429fac1083f88
Jordain <jordainjoyce@hotmail.com>
2024-06-14 11:12:21 -0400

    Updated toast msg and border on txt_gen

--- static/js/toast.js
+++ static/js/toast.js
@@ -5,29 +5,22 @@ function addToast(header, body, is_error = false, timeout = 5000) {
 
   let new_toast = document.createElement("div");
   new_toast.id = `toast-${toast_id}`;
-  new_toast.classList.add("toast");
+  new_toast.classList.add("drop-shadow-2xl", "flex", "flex-col", "py-5", "px-5", "rounded", "text-black", "text-4xl", "bg-success", "mb-4");
 
-  toast_container.classList.add("bg-success");
   if (is_error) {
-    new_toast.classList.add("toast--error");
-    toast_container.classList.add("bg-error");
+    new_toast.classList.replace("bg-success", "bg-error");
   }
 
   let toast_header_row = document.createElement("div");
-  toast_header_row.classList.add("toast-header-row");
-  toast_header_row.classList.add("text-4xl");
-  toast_header_row.classList.add("text-black");
-
+  toast_header_row.classList.add("flex", "justify-between", "items-center", "py-2");
 
   let toast_header = document.createElement("div");
-  toast_header.classList.add("toast-header");
   toast_header.innerHTML = header;
 
   let toast_close_button = document.createElement("button");
-  toast_close_button.classList.add("toast-close-button");
-  toast_close_button.innerHTML = "🗙";
+  toast_close_button.classList.add("text-xl", "font-bold");
+  toast_close_button.innerHTML = "✕";
   toast_close_button.addEventListener("click", (event) => {
-    // new_toast.classList.add('toast-leave-animation');
     new_toast.style.marginBottom = `-${new_toast.offsetHeight + 24}px`;
     new_toast.style.opacity = `0`;
     new_toast.style.transform = `translateX(512px)`;
@@ -38,6 +31,7 @@ function addToast(header, body, is_error = false, timeout = 5000) {
   });
 
   let toast_body = document.createElement("p");
+  toast_body.classList.add("mt-2", "text-xl");
   toast_body.innerHTML = body;
 
   toast_header_row.appendChild(toast_header);
@@ -56,6 +50,6 @@ function addToast(header, body, is_error = false, timeout = 5000) {
 var idx = 0;
 
 function testToast() {
-  addToast(`toast #${idx}`, "This is a test message!");
+  addToast(`Success`, `toast #${idx}: This is a test message!`);
   idx += 1;
 }
